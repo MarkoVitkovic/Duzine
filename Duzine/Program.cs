6e4a5e52a87ee2ab8cbacaea19cddd3116e5ed25
Marko Vitković <mavitkov@vsite.hr>
2020-01-17 00:38:11 +0100

    dodao listu i u nju ubacio operand, tj razdjelio ih, trenutno je hardkodirano.

--- Duzine/Program.cs
+++ Duzine/Program.cs
@@ -7,105 +7,58 @@ using System.Threading.Tasks;
 
 namespace Duzine
 {
-	public class Duzina
+
+	class Program
 	{
-
-		public static Duzina Parse(string s, NumberStyles styles, IFormatProvider provider)
+		public static Program Parse(string s)
 		{
-			Duzina duz = new Duzina();
+			Program duz = new Program();
 
-			if (s.TrimEnd(null).EndsWith("mm"))
-			{
-				duz.Value = Double.Parse(s.Remove(s.LastIndexOf("m"), 2), styles, provider);
-			}
-			if (s.TrimEnd(null).EndsWith("cm"))
-			{
-				duz.Value = Double.Parse(s.Remove(s.LastIndexOf("c"), 2), styles, provider);
-			}
-			if (s.TrimEnd(null).EndsWith("m"))
-			{
-				duz.Value = Double.Parse(s.Remove(s.LastIndexOf("m"), 1), styles, provider);
-			}
-			if (s.TrimEnd(null).EndsWith("km"))
-			{
-				duz.Value = Double.Parse(s.Remove(s.LastIndexOf("k"), 2), styles, provider);
-			}
+			
 
 			return duz;
 		}
 
-		private Duzina() { }
-
-		protected double m_value;
-
-		public double Value
-		{
-			get { return m_value; }
-			private set { m_value = value; }
-		}
-		public double mm
-		{
-			get { return (m_value / 1000); }
-			private set { m_value = value * 1000; }
-		}
-
-		public double cm
-		{
-			get { return (m_value / 100); }
-			private set { m_value = value * 100; }
-		}
-		public double km
-		{
-			get { return (m_value / 1000); }
-			private set { m_value = value / 1000; }
-		}
-
-		public double m
-		{
-			get { return m_value; }
-			private set { m_value = value; }
-		}
-	}
-	class Program
-	{
+		private Program() { }
 
 		static void Main(string[] args)
 		{
 			string unos;
 			string mj_fin;
-			NumberStyles styles;
-			//Komentar
-			IFormatProvider provider;
-			Duzina duz;
 			Console.WriteLine("Molimo unesite duzine koje zelite zbrojiti(oduzeti).\n");
-			unos = Console.ReadLine();
-			Console.WriteLine("\n");
-			Console.WriteLine("Molimo unesite mjernu jedinicu u kojoj zelite rezultat(mm,cm,m,km).\n");
-			mj_fin = Console.ReadLine();
-			Console.WriteLine("\n");
-			styles = NumberStyles.Float;
-			provider = CultureInfo.CreateSpecificCulture("fr-FR");
-			duz = Duzina.Parse(unos, styles, provider);
-
-			switch (mj_fin)
+			unos = "3m + 45cm - 38mm";
+			Program duzina = new Program();
+			char[] spearator = { '+', '-' };
+			String[] strlist = unos.Split(spearator, StringSplitOptions.RemoveEmptyEntries);
+			foreach (String s in strlist)
 			{
-				case "mm":
-					Console.WriteLine("{0}mm je {1}m.", duz.mm, duz.Value);
-					break;
-				case "cm":
-					Console.WriteLine("{0}cm je {1}m.", duz.cm, duz.Value);
-					break;
-				case "m":
-					Console.WriteLine("{0}m je {1}m.", duz.m, duz.Value);
-					break;
-				case "km":
-					Console.WriteLine("{0}km je {1}m.", duz.km, duz.Value);
-					break;
-				default:
-					Console.WriteLine("Pogresan unos");
-					break;
+				Console.WriteLine(s.Length);
 			}
+			//Console.WriteLine("\n");
+			//Console.WriteLine("Molimo unesite mjernu jedinicu u kojoj zelite rezultat(mm,cm,m,km).\n");
+			//mj_fin = Console.ReadLine();
+			//Console.WriteLine("\n");
+
+			//switch (mj_fin)
+			//{
+			//	case "mm":
+			//		Console.WriteLine("{0}mm je {1}m.", duz.mm, duz.Value);
+			//		break;
+			//	case "cm":
+			//		Console.WriteLine("{0}cm je {1}m.", duz.cm, duz.Value);
+			//		break;
+			//	case "m":
+			//		Console.WriteLine("{0}m je {1}m.", duz.m, duz.Value);
+			//		break;
+			//	case "km":
+			//		Console.WriteLine("{0}km je {1}m.", duz.km, duz.Value);
+			//		break;
+			//	default:
+			//		Console.WriteLine("Pogresan unos");
+			//		break;
+			//}
 			Console.ReadKey();
+
 
 		}
 	}
